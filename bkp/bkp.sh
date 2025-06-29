0010154e0ed15fcd094445dab2f8058f86716e3c
jdetok <jdeko17@gmail.com>
2025-06-29 16:06:15 -0500

    cleanup func in bkp file

--- bkp/bkp.sh
+++ bkp/bkp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 BKP_PATH=/mnt/store/bkp/linux/pi-jdeto
 BKP_DIR=${BKP_PATH}/bkp_$(date +"%m%d%Y_%H%M%S")
+MAXD=3
+
 
 mkdir -p $BKP_DIR
 
@@ -34,6 +36,25 @@ function cpetc() {
 	sudo cp /etc/$1 $BKP_DIR/etc/$f
 }
 
+# rm old bkp dirs if necessary
+function cleanup() {
+	ndirs=$(find $BKP_PATH -maxdepth 1 -type d -name 'bkp_*'  | wc -l)
+	echo "number of bkp directories: $ndirs"
+	
+	if [ $ndirs -gt $MAXD ]; then # number of old dirs to rm
+		n=$(($ndirs - $MAXD))
+		i=0
+		echo "attempting to remove $n old directories..."
+		find $BKP_PATH -maxdepth 1 -type d -name 'bkp_*' | \
+		sort | head -n $n | \
+		while read oldestdir; do
+			echo "removing ${oldestdir}..."
+			sudo rm -r $oldestdir
+		done
+	fi
+}
+
+# copy hidden dotfiles in the home dir
 cpdotf
 
 # backup home directory
@@ -49,5 +70,9 @@ cpetc fstab
 cpetc passwd
 cpetc group
 cpetc vim/vimrc
+
+# remove old backup directories
+echo -e "\nstarting cleanup..."
+cleanup
 
 echo -e "\nbackup complete at $BKP_DIR"
---
+++ nas/mntnas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# TODO - add the necessary lines to mount nas shares to /etc/fstab
+
+function mntsmb() {
+	local share
+	echo "mounting smb share..."
+
+}
+
+function mntnfs() {
+	echo "mounting smb share..."
+}
+
+function mntnas() {	
+	echo "mounting smb share..."
+}
